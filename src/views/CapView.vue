8282e9cd26dc44a73f95403e8bd6e777f6a3c0e5
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2023-03-17 22:09:32 +0100

    test upload affiche

--- src/views/CapView.vue
+++ src/views/CapView.vue
@@ -22,7 +22,13 @@
     <div class="block sm:hidden">
       <h2 class="reveal ml-6 text-7xl font-bold tracking-tight sm:text-9xl">Art direction.</h2>
       <div class="flex flex-col items-center">
-        <div class="mt-3 w-9/12"><img class="object-cover" src="/image/Cap/Cover.jpg" alt="" /></div>
+        <div class="mt-3 w-9/12">
+          <img class="object-cover" src="/image/Cap/Cover.jpg" alt="" @click="showOverlay(1)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown">
+            <img class="object-contain" :src="overlayImageSrc" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
+        </div>
         <div class="flex w-11/12 flex-col justify-between">
           <p class="reveal mt-6 text-justify text-xs transition-opacity duration-500">
             For the release of the first project of the promising Parisian rapper Deemax, I was in charge of the entire art direction of the
@@ -39,7 +45,13 @@
     <!--VERSION Tablette -->
     <div class="hidden sm:block lg:hidden">
       <div class="flex gap-5">
-        <div class="w-2/3 lg:w-1/2"><img class="object-cover lg:w-3/4" src="/image/Cap/Cover.jpg" alt="" /></div>
+        <div class="w-2/3 lg:w-1/2">
+          <img class="object-cover lg:w-3/4" src="/image/Cap/Cover.jpg" alt="" @click="showOverlay(1)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc1" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
+        </div>
         <div class="flex w-1/3 flex-col justify-between lg:w-1/2">
           <p class="reveal mt-6 text-justify text-xs opacity-100 duration-1000 lg:transition-opacity">
             For the release of the first project of the promising Parisian rapper Deemax, I was in charge of the entire art direction of the
@@ -54,8 +66,14 @@
     <!--VERSION PC -->
     <div class="hidden lg:block">
       <div class="flex gap-5">
-        <div class="w-2/3 lg:w-1/2"><img class="object-cover lg:w-3/4" src="/image/Cap/Cover.jpg" alt="" /></div>
-        <div class="flex w-1/3 flex-col justify-between gap-3 lg:w-1/2">
+        <div class="w-2/3 lg:w-1/2">
+          <img class="object-cover lg:w-3/4" src="/image/Cap/Cover.jpg" alt="" @click="showOverlay(1)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc1" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
+        </div>
+        <div class="gap- flex w-1/3 flex-col justify-between gap-0 lg:w-1/2">
           <p class="mt-6 text-justify text-xs opacity-100 duration-1000 lg:opacity-0 lg:transition-opacity" :id="descriptionId">
             For the release of the first project of the promising Parisian rapper Deemax, I was in charge of the entire art direction of the
             project. The goal was to create a strong imagery, reminding the old school aspect of Deemax's music, while being a subtle
@@ -72,7 +90,7 @@
           >
             Art direction.
           </h2>
-          <p class="order-last -mt-14 text-xs uppercase">( Move the mouse over the titles to learn more )</p>
+          <p class="text-xs uppercase">( Move the mouse over the titles to learn more )</p>
         </div>
       </div>
     </div>
@@ -81,7 +99,11 @@
     <div class="block lg:hidden">
       <div class="mt-24 flex flex-col">
         <div class="flex w-full flex-row-reverse items-center gap-6">
-          <img class="h-52 w-1/2 object-cover" src="/image/Cap/MédiaDeemax.jpg" alt="" />
+          <img class="h-52 w-1/2 object-cover" src="/image/Cap/MédiaDeemax.jpg" alt="" @click="showOverlay(2)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown2">
+            <img class="object-contain sm:w-1/2" :src="overlayImageSrc2" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
           <p class="reveal mt-6 w-1/2 px-3 text-justify text-xs duration-500">
             The project required a lot of work and some of the interesting proposals that did not fit into the framework of an album cover
             (lacking visual imact) were reused for the interviews with the media.
@@ -89,7 +111,11 @@
         </div>
         <div class="flex w-full flex-row-reverse items-end gap-6">
           <h2 class="reveal w-1/2 cursor-pointer text-3xl font-bold tracking-tight sm:text-7xl">Media visual.</h2>
-          <img class="h-52 w-1/2 object-cover object-center" src="/image/Cap/DEEV8.jpg" alt="" />
+          <img class="h-52 w-1/2 object-cover object-center" src="/image/Cap/DEEV8.jpg" alt="" @click="showOverlay(3)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown3">
+            <img class="object-contain sm:w-1/2" :src="overlayImageSrc3" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
         </div>
       </div>
     </div>
@@ -98,7 +124,11 @@
     <div class="hidden lg:block">
       <div class="mt-40 flex flex-col">
         <div class="flex w-full flex-row-reverse items-center gap-6">
-          <img class="h-52 w-1/2 object-cover" src="/image/Cap/MédiaDeemax.jpg" alt="" />
+          <img class="h-52 w-1/2 object-cover" src="/image/Cap/MédiaDeemax.jpg" alt="" @click="showOverlay(2)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown2">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc2" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
           <p class="mt-6 w-1/2 pr-52 pl-9 text-justify text-xs opacity-0 transition-opacity duration-500" :id="descriptionId2">
             The project required a lot of work and some of the interesting proposals that did not fit into the framework of an album cover
             (lacking visual imact) were reused for the interviews with the media.
@@ -113,7 +143,11 @@
           >
             Media visual.
           </h2>
-          <img class="h-52 w-1/2 object-cover object-center pr-32" src="/image/Cap/DEEV8.jpg" alt="" />
+          <img class="h-52 w-1/2 object-cover object-center pr-32" src="/image/Cap/DEEV8.jpg" alt="" @click="showOverlay(3)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown3">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc3" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
         </div>
       </div>
     </div>
@@ -123,21 +157,33 @@
       <div class="mt-28 flex">
         <div class="w-1/2 pr-4">
           <h2 class="reveal h-1/3 pt-7 text-4xl font-bold tracking-tight sm:text-6xl">Project extensions.</h2>
-          <img class="mb-6 w-full object-cover" src="/image/Cap/Tracklist.jpg" alt="" />
+          <img class="mb-6 w-full object-cover" src="/image/Cap/Tracklist.jpg" alt="" @click="showOverlay(5)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown5">
+            <img class="object-contain sm:w-1/2" :src="overlayImageSrc5" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
           <p class="reveal w-full text-justify text-xs duration-500">
             I also took care of the Tracklist which is an extension of the cover with the graphic style of the latter. The whole was
             realized in order to conceive soon physical CD of the project.
           </p>
         </div>
         <div class="w-1/2 pl-4">
-          <img class="mb-6 w-full object-cover" src="/image/Cap/DEEM.jpg" alt="" />
+          <img class="mb-6 w-full object-cover" src="/image/Cap/DEEM.jpg" alt="" @click="showOverlay(4)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown4">
+            <img class="object-contain sm:w-1/2" :src="overlayImageSrc4" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
           <p class="reveal w-full text-justify text-xs duration-500">
             The artistic direction of the album was also accompanied by a visual clip animated by me, which was released on my own accounts
             for reasons of timing. The clip which I directed the scenario, the editing, as well as the illustrations had for goal to take
             again this artistic direction contrasting the black and white in a slightly different style to evoke the topic of the project
             under a similar angle, but which is not repetitive.
           </p>
-          <img class="mt-6 mb-6 w-full object-cover" src="/image/Cap/DEEANIM.jpg" alt="" />
+          <img class="mt-6 mb-6 w-full object-cover" src="/image/Cap/DEEANIM.jpg" alt="" @click="showOverlay(6)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown6">
+            <img class="object-contain sm:w-1/2" :src="overlayImageSrc6" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
         </div>
       </div>
     </div>
@@ -146,12 +192,20 @@
     <div class="hidden lg:block">
       <div class="mt-52 flex flex-col">
         <div class="flex w-full flex-row-reverse items-center gap-0">
-          <img class="h-60 w-1/3 object-cover" src="/image/Cap/DEEM.jpg" alt="" />
+          <img class="h-60 w-1/3 object-cover" src="/image/Cap/DEEM.jpg" alt="" @click="showOverlay(4)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown4">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc4" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
           <p class="mt-6 w-1/3 px-12 pl-9 text-justify text-xs opacity-0 transition-opacity duration-500" :id="descriptionId3">
             I also took care of the Tracklist which is an extension of the cover with the graphic style of the latter. The whole was
             realized in order to conceive soon physical CD of the project.
           </p>
-          <img class="h-60 w-1/3 object-cover object-top" src="/image/Cap/Tracklist.jpg" alt="" />
+          <img class="h-60 w-1/3 object-cover object-top" src="/image/Cap/Tracklist.jpg" alt="" @click="showOverlay(5)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown5">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc5" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
         </div>
         <div class="flex w-full flex-row-reverse items-end">
           <p class="w-1/3 pl-9 pb-6 pr-20 text-justify text-xs opacity-0 transition-opacity duration-500" :id="descriptionId4">
@@ -160,7 +214,11 @@
             again this artistic direction contrasting the black and white in a slightly different style to evoke the topic of the project
             under a similar angle, but which is not repetitive.
           </p>
-          <img class="h-60 w-1/3 object-cover object-center" src="/image/Cap/DEEANIM.jpg" alt="" />
+          <img class="h-60 w-1/3 object-cover object-center" src="/image/Cap/DEEANIM.jpg" alt="" @click="showOverlay(6)" />
+          <div class="overlay bg-black bg-opacity-95" v-if="overlayShown6">
+            <img class="w-1/2 object-contain" :src="overlayImageSrc6" @click="hideOverlay" />
+            <p class="text-sm uppercase" @click="hideOverlay">(Click to close)</p>
+          </div>
           <h2
             class="reveal w-1/3 animate-pulse cursor-pointer text-3xl font-bold tracking-tight opacity-0 hover:animate-none sm:text-6xl"
             @click="toggleDescription"
@@ -187,9 +245,38 @@ export default {
       descriptionId3: "description-" + Math.random().toString(36).substring(7),
       descriptionId4: "description-" + Math.random().toString(36).substring(7),
       descriptionVisible: false,
+      overlayShown: false,
+      overlayShown2: false,
+      overlayShown3: false,
+      overlayShown4: false,
+      overlayShown5: false,
+      overlayShown6: false,
     };
   },
   methods: {
+    showOverlay(index) {
+      if (index === 1) {
+        this.overlayShown1 = true;
+      } else if (index === 2) {
+        this.overlayShown2 = true;
+      } else if (index === 3) {
+        this.overlayShown3 = true;
+      } else if (index === 4) {
+        this.overlayShown4 = true;
+      } else if (index === 5) {
+        this.overlayShown5 = true;
+      } else if (index === 6) {
+        this.overlayShown6 = true;
+      }
+    },
+    hideOverlay() {
+      this.overlayShown = false;
+      this.overlayShown2 = false;
+      this.overlayShown3 = false;
+      this.overlayShown4 = false;
+      this.overlayShown5 = false;
+      this.overlayShown6 = false;
+    },
     toggleDescription() {
       this.descriptionVisible = !this.descriptionVisible;
     },
@@ -261,6 +348,26 @@ export default {
       }
     },
   },
+  computed: {
+    overlayImageSrc1() {
+      return this.overlayShown1 ? "/image/Cap/Cover.jpg" : "/image/Cap/Cover.jpg";
+    },
+    overlayImageSrc2() {
+      return this.overlayShown2 ? "/image/Cap/MédiaDeemax.jpg" : "/image/Cap/MédiaDeemax.jpg";
+    },
+    overlayImageSrc3() {
+      return this.overlayShown3 ? "/image/Cap/DEEV8.jpg" : "/image/Cap/DEEV8.jpg";
+    },
+    overlayImageSrc4() {
+      return this.overlayShown4 ? "/image/Cap/DEEM.jpg" : "/image/Cap/DEEM.jpg";
+    },
+    overlayImageSrc5() {
+      return this.overlayShown5 ? "/image/Cap/Tracklist.jpg" : "/image/Cap/Tracklist.jpg";
+    },
+    overlayImageSrc6() {
+      return this.overlayShown6 ? "/image/Cap/DEEANIM.jpg" : "/image/Cap/DEEANIM.jpg";
+    },
+  },
   mounted() {
     window.addEventListener("scroll", this.reveal);
   },
@@ -281,6 +388,19 @@ export default {
 }
 #description {
   display: none;
+}
+
+.overlay {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
 }
 </style>
 
--- src/views/DeemaxView.vue
+++ src/views/DeemaxView.vue
@@ -1,51 +1,72 @@
 <template>
-  <div class="w-full pt-28">
-    <img src="/image/Cap/Cover.jpg" alt="" class="object-cover" @click="showImageOverlay('/image/Cap/Cover.jpg')" />
-  </div>
-  <div id="image-overlay" @click="hideImageOverlay()">
-    <img id="overlay-image" :src="overlayImageSrc" />
+  <div class="h-4/6 bg-blue-400 sm:h-4/6">
+    <img class="h-1/3 w-full object-cover" src="/image/Cap/Cover.jpg" alt="" @click="showOverlay(1)" />
+    <div class="overlay" v-if="overlayShown1">
+      <img class="object-contain" :src="overlayImageSrc1" @click="hideOverlay" />
+    </div>
+    <img class="h-1/3 w-full object-cover" src="/image/Cap/DEEM.jpg" alt="" @click="showOverlay(2)" />
+    <div class="overlay" v-if="overlayShown2">
+      <img class="object-contain" :src="overlayImageSrc2" @click="hideOverlay" />
+    </div>
+    <img class="h-1/3 w-full object-cover" src="/image/Cap/DEEV8.jpg" alt="" @click="showOverlay(3)" />
+    <div class="overlay" v-if="overlayShown3">
+      <img class="object-contain" :src="overlayImageSrc3" @click="hideOverlay" />
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      overlayImageSrc: "",
-      overlayVisible: false,
-    };
-  },
-  methods: {
-    showImageOverlay(imageSrc) {
-      this.overlayImageSrc = imageSrc;
-      this.overlayVisible = true;
-    },
-    hideImageOverlay() {
-      this.overlayVisible = false;
-    },
-  },
-};
-</script>
-
-<style scoped>
-#image-overlay {
-  display: none;
+<style>
+.overlay {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.9);
-  z-index: 9999;
-}
-
-#overlay-image {
-  max-width: 90%;
-  max-height: 90%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      overlayShown1: false,
+      overlayShown2: false,
+      overlayShown3: false,
+    };
+  },
+  methods: {
+    showOverlay(index) {
+      if (index === 1) {
+        this.overlayShown1 = true;
+      } else if (index === 2) {
+        this.overlayShown2 = true;
+      } else if (index === 3) {
+        this.overlayShown3 = true;
+      }
+    },
+    hideOverlay() {
+      this.overlayShown1 = false;
+      this.overlayShown2 = false;
+      this.overlayShown3 = false;
+    },
+  },
+  computed: {
+    overlayImageSrc1() {
+      return this.overlayShown1 ? "/image/Cap/Cover.jpg" : "";
+    },
+    overlayImageSrc2() {
+      return this.overlayShown2 ? "/image/Cap/DEEM.jpg" : "";
+    },
+    overlayImageSrc3() {
+      return this.overlayShown3 ? "/image/Cap/DEEV8.jpg" : "";
+    },
+  },
+};
+</script>
+
 
