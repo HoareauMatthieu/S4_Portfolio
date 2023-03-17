1ed3d8c05ec9941c602435740f4082d0910497bc
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2023-03-17 20:24:49 +0100

    test click

--- src/views/DeemaxView.vue
+++ src/views/DeemaxView.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="relative w-full pt-56">
-    <img src="/image/Cap/Cover.jpg" alt="" class="h-44 w-full object-cover" />
-    <div class="overlay absolute inset-0 hidden">
-      <img src="/image/Cap/Cover.jpg" alt="" class="h-full w-full object-contain" />
-    </div>
+  <div class="w-full pt-28">
+    <img src="/image/Cap/Cover.jpg" alt="" class="object-cover" @click="showImageOverlay('/image/Cap/Cover.jpg')" />
+  </div>
+  <div id="image-overlay" @click="hideImageOverlay()">
+    <img id="overlay-image" :src="overlayImageSrc" />
   </div>
 </template>
 
-<style>
-.overlay {
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: opacity 0.2s ease-in-out;
-  opacity: 0;
+<script>
+export default {
+  data() {
+    return {
+      overlayImageSrc: "",
+      overlayVisible: false,
+    };
+  },
+  methods: {
+    showImageOverlay(imageSrc) {
+      this.overlayImageSrc = imageSrc;
+      this.overlayVisible = true;
+    },
+    hideImageOverlay() {
+      this.overlayVisible = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#image-overlay {
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.9);
+  z-index: 9999;
 }
 
-.overlay img {
-  max-width: 80%;
-  max-height: 80%;
-}
-
-.overlay:hover {
-  opacity: 1;
+#overlay-image {
+  max-width: 90%;
+  max-height: 90%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
 
